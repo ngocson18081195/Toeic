8c4c8f09542b4de010daf16ae2f6d9b567ee268b
ngocson18081195 <ngocson18081195@gmail.com>
2017-11-20 19:43:48 +0700

    Project

---
+++ src/main/java/toeic/ToeicApplication.java
@@ -0,0 +1,12 @@
+package toeic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToeicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToeicApplication.class, args);
+	}
+}
